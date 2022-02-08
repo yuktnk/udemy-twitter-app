fcab10145a60edd423a0d849204aabad1c0862ba
yuktnk <yuki0wl@gmail.com>
2022-02-08 18:45:17 +0900

    firestoreにデータを保存できるように

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -74,13 +74,14 @@ const Auth: React.FC = () => {
     const authUser = await auth.createUserWithEmailAndPassword(email, password);
     let url = "";
     if (avatarImage) {
-      const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // ランダムな文字列を作るための元になる候補の文字 ちなみに62文字ある
       const N = 16;
       const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
         .map((n) => S[n % S.length])
         .join("");
       const fileName = randomChar + "_" + avatarImage.name;
 
+      await storage.ref(`avatars/${fileName}`).put(avatarImage);
       url = await storage.ref("avatars").child(fileName).getDownloadURL();
     }
     await authUser.user?.updateProfile({
@@ -119,6 +120,37 @@ const Auth: React.FC = () => {
             {isLogin ? "Login" : "Register"}
           </Typography>
           <form className={classes.form} noValidate>
+            {!isLogin && (
+              <>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                  value={username}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setUsername(e.target.value);
+                  }}
+                />
+                <Box textAlign="center">
+                  <IconButton>
+                    <label>
+                      <AccountCircleIcon
+                        fontSize="large"
+                        className={avatarImage ? styles.login_addIconLoaded : styles.login_addIcon}
+                      />
+                      <input className={styles.login_hiddenIcon} type="file" onChange={onChangeImagehandler} />
+                    </label>
+                  </IconButton>
+                </Box>
+              </>
+            )}
+
             <TextField
               variant="outlined"
               margin="normal"
@@ -147,6 +179,9 @@ const Auth: React.FC = () => {
             />
 
             <Button
+              disabled={
+                isLogin ? !email || password.length < 6 : !username || !email || password.length < 6 || !avatarImage
+              }
               fullWidth
               variant="contained"
               color="primary"
