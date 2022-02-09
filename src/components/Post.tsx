c3d3644a8d4416b2b8caab3f529d2305baa85b58
yuktnk <yuki0wl@gmail.com>
2022-02-09 16:18:32 +0900

    コメント機能を実装

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -19,6 +19,20 @@ interface PROPS {
 }
 
 const Post: React.FC<PROPS> = (props) => {
+  const user = useAppSelector(selectUser);
+  const [comment, setComment] = useState("");
+
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection("posts").doc(props.postId).collection("comments").add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
+
   return (
     <li className={styles.post}>
       <div className={styles.post_avatar}>
@@ -41,6 +55,24 @@ const Post: React.FC<PROPS> = (props) => {
             <img src={props.image} alt="tweet" />
           </figure>
         )}
+        <form onSubmit={newComment}>
+          <div className={styles.post_form}>
+            <input
+              className={styles.post_input}
+              type="text"
+              placeholder="Type new comment"
+              value={comment}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setComment(e.target.value)}
+            />
+            <button
+              disabled={!comment}
+              className={comment ? styles.post_button : styles.post_buttonDisable}
+              type="submit"
+            >
+              <SendIcon className={styles.post_sendIcon} />
+            </button>
+          </div>
+        </form>
       </div>
     </li>
   );
