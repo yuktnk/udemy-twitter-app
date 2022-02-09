2ca27e200294003f9c633d02b9169c62c685ef4f
yuktnk <yuki0wl@gmail.com>
2022-02-09 12:58:45 +0900

    Firestoreへのデータ登録ロジックを作成

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TweetInput.module.css";
 import { useAppSelector } from "../app/hooks";
 import { selectUser } from "../features/userSlice";
-import { auth } from "../firebase";
-import { Avatar } from "@material-ui/core";
+import { auth, storage, db } from "../firebase";
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import firebase from "firebase/app";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
 const TweetInput = () => {
   const user = useAppSelector(selectUser);
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const [tweetMsg, setTweetMsg] = useState("");
+
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Reloadをしないようにする
+    if (tweetImage) {
+      const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // ランダムな文字列を作るための元になる候補の文字 ちなみに62文字ある
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + tweetImage.name;
+
+      const uploadTweetImage = storage.ref(`images/${fileName}`).put(tweetImage);
+      uploadTweetImage.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        () => {},
+        (err) => {
+          alert(err.message);
+        },
+        async () => {
+          await storage
+            .ref("images")
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMsg,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              });
+            });
+        }
+      );
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMsg,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setTweetImage(null);
+    setTweetMsg("");
+  };
 
   return (
     <div>
